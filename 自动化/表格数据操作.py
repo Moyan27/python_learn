4a854b473fa112e544674e7c15e7a5c9dd7efecb
zhanyan <1614116381@qq.com>
2022-10-25 20:45:07 +0800

    g

--- 自动化/表格数据操作.py
+++ 自动化/表格数据操作.py
@@ -2,10 +2,9 @@ import pandas as pd
 import os 
 
 path=os.path.dirname(__file__)
-new_path1=path+'/1.xlsx'
-df1=pd.read_excel(new_path1,sheet_name='Sheet1')
-new_path2=path+'/1.csv'
-df2=pd.read_csv(new_path2)
 
+# df=pd.read_csv('{}/1.csv'.format(path))
+# print(df)
 
-print(df2)
+df = pd.read_excel('{}/1.xlsx'.format(path),sheet_name='Sheet1')
+print(df)
