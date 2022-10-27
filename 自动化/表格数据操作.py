373a71f796682c99d54724287425ad0450d69e5d
zhanyan <1614116381@qq.com>
2022-10-27 22:00:05 +0800

    更新

--- 自动化/表格数据操作.py
+++ 自动化/表格数据操作.py
@@ -6,5 +6,5 @@ path=os.path.dirname(__file__)
 # df=pd.read_csv('{}/1.csv'.format(path))
 # print(df)
 
-df = pd.read_excel('{}/1.xlsx'.format(path),sheet_name='Sheet1')
-print(df)
+df1= pd.read_excel('{}/1.xlsx'.format(path),sheet_name='Sheet1')
+print(df1)
