99472f0e776f75c36be80079a18b17828e9429e4
zhanyan <1614116381@qq.com>
2022-10-31 10:28:35 +0000

    gg

--- 爬虫/wallpaper_get.py
+++ 爬虫/wallpaper_get.py
@@ -78,7 +78,7 @@ class Wallpaper(object):
         self.build_savepath()
         print('{:-^20}'.format('正在整合数据'))
         self.get_data()
-        #print(self.data_url_list)
+        print(len(list(set(self.data_url_list))))
         print('{:-^20}'.format('整合完成--尝试爬取'))
         print('{:-^20}'.format('正在多线程爬取'))
         self.built_threading()
