e05ca6fe2282e0750cca96bca06911da4a9b623b
zhanyan <1614116381@qq.com>
2022-11-07 10:49:58 +0800

    gx

--- 无聊制作/二维码生成器.py
+++ 无聊制作/二维码生成器.py
@@ -1,8 +1,6 @@
 import pyqrcode
 import os
-
 path=os.path.dirname(__file__)
-
 s=input('输入url或字符串：')
 url=pyqrcode.create(s)
-url.png('{}/hailong.png'.format(path),scale=8,background=[0xff,0xff,0xff])
+url.png('{}/shiping.png'.format(path),scale=8,background=[0xff,0xff,0xff])
