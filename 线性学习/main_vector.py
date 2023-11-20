604ee5206b6c7821a449a6992f2ecf99d32b9f13
zhanyan <1614116381@qq.com>
2023-11-20 12:56:19 +0800

    gg

--- 线性学习/main_vector.py
+++
@@ -1,9 +0,0 @@
-from XianDai.Vector import Vector
-
-
-if __name__ == '__main__':
-    vec1 =Vector([1,3])
-    vec2 = Vector([4, 3])
-    print(vec1.normalize().norm())
-    zero=Vector.zero(2)
-    print(zero.normalize())
