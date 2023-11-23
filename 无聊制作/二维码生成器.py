ab2c87e26d3f6f35dd6431e57837996376e1ee9f
zhanyan <1614116381@qq.com>
2023-11-23 16:47:58 +0800

    gx

--- 无聊制作/二维码生成器.py
+++ 无聊制作/二维码生成器.py
@@ -3,4 +3,4 @@ import os
 path=os.path.dirname(__file__)
 s=input('输入url或字符串：')
 url=pyqrcode.create(s)
-url.png('{}/shiping.png'.format(path),scale=8,background=[0xff,0xff,0xff])
+url.png('{}/1.png'.format(path),scale=8,background=[0xff,0xff,0xff])
--- 无聊制作/进度条3.py
+++ 无聊制作/进度条3.py
@@ -2,7 +2,6 @@ from alive_progress import alive_bar
 import time
 
 items= range(100)
-
 with alive_bar(len(items))as bar:
     for item in items:
         bar()
